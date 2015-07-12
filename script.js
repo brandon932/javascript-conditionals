59e38e633561f07570d403ee0dc6c53a4eb8b649
brandon932 <bthornton_932@yahoo.com>
2015-07-12 10:39:01 -0600

    basic script to to prompt for a phone number.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-var number=prompt("please enter your phone number with dashes- ex. 'xxx-xxx-xxxx'")
-
-
+var number = prompt("please enter your phone number with dashes- ex. 'xxx-xxx-xxxx'");
+if (number[3,7] != "-"){
+  number = prompt("Your number was incorrect, Please re enter it";
+      }
+      else {
+        alert "Thank you your number is correct";
+      };
 
