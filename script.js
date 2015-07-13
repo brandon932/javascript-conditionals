af5eea587b63b8e608b828d74909b1f9c28e28f8
brandon932 <bthornton_932@yahoo.com>
2015-07-13 00:14:55 -0600

    rewrote code without braces and actually works this time.

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-var number = prompt("please enter your phone number with dashes- ex. 'xxx-xxx-xxxx'");
-if (number[3,7] != "-"){
-  number = prompt("Your number was incorrect, Please re enter it";
-      }
-      else {
-        alert "Thank you your number is correct";
-      };
-
+var number = prompt("please enter your phone numb with dashes- ex. 'xxx-xxx-xxxx'");
+console.log(number);
+console.log(number.length);
+if (number.length === 12)
+    if (number.charAt(3&&7)==="-")
+      console.log("good job");
+else console.log("please reenter your number");
