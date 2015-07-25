f51c1b2f5a2b5de1e3f92da949afbdc94385fb9f
brandon932 <bthornton_932@yahoo.com>
2015-07-25 14:32:45 -0600

    refactored first two functions and added final functions

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
-var number = prompt("please enter your phone numb with dashes- ex. 'xxx-xxx-xxxx'");
+function phoneNumber(number){
 while (number.length !== 12){
-  number = prompt("please reenter your number");
-}
+  number = prompt("please reenter your number and use dashes");
+  }
 if (number.charAt(3&&7)==="-")
   alert("Your phone number looks correct");
-var birthday = prompt("Please enter your birthday xx/xx/xx");
+}
+
+function birthdayDate(birthday){
 while (birthday.length !== 8) {
-  birthday = prompt("please reenter your birthday");
+  birthday = prompt("please use the format xx/xx/xx");
 }
 if (birthday.charAt(2&&5)==="/") {
-  prompt("Awesome!");
-}
-var postalCode = prompt("enter your postal code as xxxxx or xxxxx-xxxx");
-while (postalCode.length !== 8 || 10) {
-  postalCode = prompt("reenter your postal code");
-}
-if (postalCode.length ===10) {
-  if (postalCode.charAt(8==="-")) {
-    alert("Good job");
-  }else {
-    alert("good job");
+  alert("Awesome!");
   }
 }
-// var birth = prompt("please enter your birthday as xx/xx/xx")
-// if (birth.charAt === "/")
-//
-// TODO:
-    // postal code (xxxxx OR xxxxx-xxxx)
-    // state abbreviation (XX)
-    // married ("yes" or "no" or "YES" or "NO" or "Yes" or "No")
+
+function checkPostal(postalCode){
+  if (postalCode.length === 5 || postalCode.length === 10){
+    if (postalCode.length > 5){
+      if (postalCode.charAt(5)==="-") {
+        alert("thank you for the full zip code");
+      }else{
+        checkPostal(prompt("that is not correct please enter your postal code with a dash"));
+      }
+    }else{
+      alert("good job");
+    }
+  }else{
+    checkPostal(prompt("that is not correct please enter your postal code"));
+    }
+  }
+
+function checkAbbreviation(state){
+  if (state.length === 2){
+    if(state === state.toUpperCase()){
+      alert("Great!");
+    }else{
+      checkAbbreviation(prompt("please capitalize the abbreviation"));
+    }
+  }else{
+    checkAbbreviation(prompt("please use the abbreviation"));
+  }
+}
+
+function isMarried(answer){
+  if (answer.toLowerCase() === "yes" || answer.toLowerCase() === "no"){
+    alert("Good for you");
+  }else{
+    isMarried(prompt("Answer yes or no."));
+  }
+}
+
+phoneNumber(prompt("please enter your phone numb with dashes- ex. 'xxx-xxx-xxxx'"));
+birthdayDate(prompt("Please enter your birthday "));
+checkPostal(prompt("please enter your postal code"));
+checkAbbreviation(prompt("What state do you live in?"));
+isMarried(prompt("Are you Married yes or no?"));
